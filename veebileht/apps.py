27ca7f714fba233b51e201f816bec6cdb914d847
Robert Matsar <robert.matsar@gmail.com>
2021-11-07 00:30:52 +0200

    added django boilerplate code

---
+++ veebileht/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+#this is actually the config file - robert
+
+class VeebilehtConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'veebileht'
