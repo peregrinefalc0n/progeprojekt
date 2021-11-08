fe8a28da5ce70f48460ff3081947d290d2c633fe
Robert Matsar <robert.matsar@gmail.com>
2021-11-08 02:30:28 +0200

    index page reference created

--- veebileht/views.py
+++ veebileht/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
 
-# req - resp 
-#this is the request handler
+#req - resp 
+#view function is a function that takes a request and returns a response
+#its a request handler
+
+def returnwebpage(request):
+    return render(request, 'index.html', {'name': 'Robert'})
